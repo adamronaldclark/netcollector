1155ad431eb1fc660bf547089a153720c7aa4e76
Adam <adam.ronald.clark@gmail.com>
2020-08-16 12:31:03 -0700

    Upload

--- netcollector.py
+++ netcollector.py
@@ -11,14 +11,14 @@ import time
 try:
     subprocess.call(["tcpdump", "--version"])
 except OSError as e:
-    print "tcpdump is not installed. Exiting as tcpdump is required to run NETCOLLECTOR."
+    print ("tcpdump is not installed. Exiting as tcpdump is required to run NETCOLLECTOR.")
     sys.exit()
 
 # Edit sys.path to include scapy import directory. Exit the program if scapy is not found.
 scapy_dir = "imports/scapy-2.4.0"
 scapy_dir_status = subprocess.call("test -e '{}'".format(scapy_dir), shell=True)
 if scapy_dir_status == 1:
-    print "scapy directory not found. Exiting as scapy is required to run NETCOLLECTOR."
+    print ("scapy directory not found. Exiting as scapy is required to run NETCOLLECTOR.")
     sys.exit()
 sys.path.insert(0, scapy_dir)
 
@@ -32,7 +32,7 @@ subprocess.call(["clear"])
 # Uncomment the line below and add iface=int to the sniff function options at the bottom of script.
 # int = ["enp0s3", "lo"]
 # Specify gateway address you want to be alerted to mac-spoofing for.
-gateway_ipv4 = "192.168.0.253"
+gateway_ipv4 = "192.168.0.1"
 gateway_ipv6 = "2001:0db8:1:1::1"
 
 # Create empty list to store discovered hosts.
@@ -54,7 +54,7 @@ for h in new_host:
     if h == "":
         pass
     else:
-        print "Reading in IPv4 ARP host: " + h
+        print ("Reading in IPv4 ARP host: " + h)
         host = h.split(",")
         try:
             disc_host = (host[0],host[1])
@@ -72,7 +72,7 @@ for h in new_host:
     if h == "":
         pass
     else:
-        print "Reading in IPv6 NS host: " + h
+        print ("Reading in IPv6 NS host: " + h)
         host = h.split(",")
         try:
             disc_host = (host[0],host[1])
@@ -100,26 +100,25 @@ def pkt_callback(pkt):
                 if ip[1] == gateway_ipv4 and ip[0] != l2src:
                     disc_date = (time.strftime("%d/%m/%Y"))
                     disc_time = (time.strftime("%H:%M:%S"))
-                    print "ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time
+                    print ("ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time)
                     hosts_log_f = open("hosts.log", "a")
                     hosts_log_f.write("ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " +
     disc_time + "\n")
         # Skip existing entries.
         elif disc_host in disc_arp_hosts_l:
-    		pass
+    	    pass
         # Add new host to log and print to console.
         else:
-    		disc_arp_hosts_l.append(disc_host)
-    		disc_date = (time.strftime("%d/%m/%Y"))
-    		disc_time = (time.strftime("%H:%M:%S"))
-    		disc_hosts_f = open("arp_hosts", "a")
-    		disc_hosts_f.write(disc_host[0] + "," + disc_host[1] + "\n")
-    		disc_hosts_f.close()
-    		print "HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time
-    		hosts_log_f = open("hosts.log", "a")
-    		hosts_log_f.write("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " +
-    disc_time + "\n")
-    		hosts_log_f.close()
+            disc_arp_hosts_l.append(disc_host)
+            disc_date = (time.strftime("%d/%m/%Y"))
+            disc_time = (time.strftime("%H:%M:%S"))
+            disc_hosts_f = open("arp_hosts", "a")
+            disc_hosts_f.write(disc_host[0] + "," + disc_host[1] + "\n")
+            disc_hosts_f.close()
+            print ("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time)
+            hosts_log_f = open("hosts.log", "a")
+            hosts_log_f.write("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time + "\n")
+            hosts_log_f.close()
     # Check if packet is IPv6 Neighbor Solicitation.
     elif pkt.type == 34525:
         l2src = pkt.src
@@ -135,29 +134,28 @@ def pkt_callback(pkt):
                 if ip[1] == gateway_ipv6 and ip[0] != l2src:
                     disc_date = (time.strftime("%d/%m/%Y"))
                     disc_time = (time.strftime("%H:%M:%S"))
-                    print "ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time
+                    print ("ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time)
                     hosts_log_f = open("hosts.log", "a")
                     hosts_log_f.write("ALERT (GW MAC CHANGE): " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " +
     disc_time + "\n")
         # Skip existing entries.
         elif disc_host in disc_ns_hosts_l:
-    		pass
+            pass
         # Add new host to log and print to console.
         else:
-    		disc_ns_hosts_l.append(disc_host)
-    		disc_date = (time.strftime("%d/%m/%Y"))
-    		disc_time = (time.strftime("%H:%M:%S"))
-    		disc_hosts_f = open("ns_hosts", "a")
-    		disc_hosts_f.write(disc_host[0] + "," + disc_host[1] + "\n")
-    		disc_hosts_f.close()
-    		print "HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time
-    		hosts_log_f = open("hosts.log", "a")
-    		hosts_log_f.write("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " +
-    disc_time + "\n")
-    		hosts_log_f.close()
+            disc_ns_hosts_l.append(disc_host)
+            disc_date = (time.strftime("%d/%m/%Y"))
+            disc_time = (time.strftime("%H:%M:%S"))
+            disc_hosts_f = open("ns_hosts", "a")
+            disc_hosts_f.write(disc_host[0] + "," + disc_host[1] + "\n")
+            disc_hosts_f.close()
+            print ("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time)
+            hosts_log_f = open("hosts.log", "a")
+            hosts_log_f.write("HOST: " + disc_host[0] + "," + disc_host[1] + " DISCOVERED ON " + disc_date + " AT " + disc_time + "\n")
+            hosts_log_f.close()
     else:
         pass
 
-print "\nNETCOLLECTOR\n"
+print ("\nNETCOLLECTOR\n")
 
 sniff(filter="icmp6 && ip6[40] == 135 || arp",store=0,prn=pkt_callback)
